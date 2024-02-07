8e8e607ccefa9227889f6f362b71f35485e1678f
Matias Piuma <piumamatias@gmail.com>
2024-02-07 20:25:14 -0300

    se agregó la libreria 'dotenv' para correr las variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
+import dotenv from 'dotenv'
 import express from 'express'
 
-const app = express()
-const PORT = 3001
+dotenv.config()
 
-app.get('/', (req, res) => {
-	res.send('Hola Mundo!')
-})
+const app = express()
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-	console.log(`Servidor corriendo en: http://localhost:${PORT}`)
+	console.log('Servidor corriendo')
 })
